258d69e34020489aff31f291bd99ba29208afa34
mousumi <dm.mousumidas@gmail.com>
2018-08-30 14:32:50 +0530

    SCripts is added inTC1

--- Scripts/Common TestCases/TC1_Login To astorSafe Web Application/Script1535439697756.groovy
+++ Scripts/Common TestCases/TC1_Login To astorSafe Web Application/Script1535439697756.groovy
@@ -19,3 +19,17 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl('http://astoria-qa.astorsafe.com/Login.aspx?ReturnUrl=%2f')
+
+WebUI.setText(findTestObject('Object Repository/Login Page/Page_astorSafe - Login/input_loginCtrlUserName'), 'admin1')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Login Page/Page_astorSafe - Login/input_loginCtrlPassword'), '9NLz+4tGZcQ=')
+
+WebUI.click(findTestObject('Object Repository/Login Page/Page_astorSafe - Login/input_loginCtrlLoginButton'))
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/astorSafeRegressionTesting.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>For regression testing  after each sprints</description>
+   <name>astorSafeRegressionTesting</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8e099138-f56f-4557-b270-de67b61d8dc1</testSuiteGuid>
+   <testCaseLink>
+      <guid>c2107406-033a-45fe-9836-081304a23fa9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common TestCases/TC1_Login To astorSafe Web Application</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>

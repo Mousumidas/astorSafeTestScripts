476d9b7470284b12462a66e884e7fcbe8fe91a9a
mousumi <dm.mousumidas@gmail.com>
2018-08-31 12:05:02 +0530

    test message

--- Test Suites/astorSafeRegressionTesting.ts
+++ Test Suites/astorSafeRegressionTesting.ts
@@ -4,7 +4,7 @@
    <name>astorSafeRegressionTesting</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-08-31T11:58:34</lastRun>
+   <lastRun>2018-08-31T12:04:00</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>

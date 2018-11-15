4f1c8cd1b9d1448fd49c4beff9cc555d9ce6387e
Mousumidas <dm.mousumidas@gmail.com>
2018-11-15 15:34:02 +0530

    test message

--- Object Repository/Page_Training Program/input_AddView Employee Master_.rs
+++ Object Repository/Page_Training Program/input_AddView Employee Master_.rs
@@ -4,24 +4,30 @@
    <name>input_AddView Employee Master_</name>
    <tag></tag>
    <elementGuidId>b6a6ecd8-cfaa-4353-821c-80f74e6c34e5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'ContentPlaceHolder1_btnAdd']</value>
+      </entry>
+   </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
       <value>input</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>type</name>
       <type>Main</type>
       <value>submit</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>name</name>
       <type>Main</type>
